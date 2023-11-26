339b83a3f4c5f0afb74f17224d3559732900f983
Álvaro Polonio Domínguez <alvaro.pold@gmail.com>
2023-11-26 21:48:43 +0100

    form modal add-bike

--- appbike-front/src/app/app.module.ts
+++ appbike-front/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +11,7 @@ import { RegisterUserFormComponent } from './components/register-user-form/regis
 import { HttpClientModule, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { PageHomeComponent } from './ui/page-home/page-home.component';
 import { BikeListComponent } from './components/bike-list/bike-list.component';
@@ -16,11 +20,14 @@ import { BikeItemByStationComponent } from './components/bike-item-by-station/bi
 import { authInterceptorProviders } from './helpers/auth.interceptor';
 import { PageFinishRideComponent } from './ui/page-finish-ride/page-finish-ride.component';
 import { DetailsUseBarComponent } from './components/details-use-bar/details-use-bar.component';
-import { ListStationComponent } from './component/list-station/list-station.component';
 import { ListAdminStationsComponent } from './components/list-admin-stations/list-admin-stations.component';
 import { ListUserStationsComponent } from './components/list-user-stations/list-user-stations.component';
 import { HorizontalNavbarComponent } from './components/horizontal-navbar/horizontal-navbar.component';
 import { AdminIssuesPageComponent } from './ui/admin-issues-page/admin-issues-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormAddBikeAdminComponent } from './components/form-add-bike-admin/form-add-bike-admin.component';
+
+
 
 @NgModule({
   declarations: [
@@ -33,11 +40,11 @@ import { AdminIssuesPageComponent } from './ui/admin-issues-page/admin-issues-pa
     BikeItemByStationComponent,
     PageFinishRideComponent,
     DetailsUseBarComponent,
-    ListStationComponent,
     ListAdminStationsComponent,
     ListUserStationsComponent,
     HorizontalNavbarComponent,
-    AdminIssuesPageComponent
+    AdminIssuesPageComponent,
+    FormAddBikeAdminComponent,
   ],
   imports: [
     BrowserModule,
@@ -47,6 +54,12 @@ import { AdminIssuesPageComponent } from './ui/admin-issues-page/admin-issues-pa
     NgbModule,
     FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatSelectModule
   ],
   providers: [authInterceptorProviders, provideHttpClient(withFetch())],
   bootstrap: [AppComponent]
--- appbike-front/src/app/components/bike-list/bike-list.component.ts
+++ appbike-front/src/app/components/bike-list/bike-list.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Bike } from '../../models/bike-list.interface';
 import { BikeService } from '../../services/bike.service';
+import { Station } from '../../models/list-all-stations';
+import { Observable, map, startWith } from 'rxjs';
+import { StationsService } from '../../services/stations.service';
+import { FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { FormAddBikeAdminComponent } from '../form-add-bike-admin/form-add-bike-admin.component';
 
 @Component({
   selector: 'app-bike-list',
   templateUrl: './bike-list.component.html',
-  styleUrl: './bike-list.component.css'
+  styleUrl: './bike-list.component.css',
 })
 export class BikeListComponent implements OnInit {
 
   listBikes: Bike[] = [];
   countBikes: number = 0;
   currentPage: number = 1;
+  filteredOptions!: Observable<Station[]>;
+  options: Station[] = [];
+  myControl = new FormControl<string | Station>('');
 
-  constructor(private bikeService: BikeService) { }
+  constructor(private bikeService: BikeService, private stationService: StationsService, public dialog: MatDialog) { }
 
 
   ngOnInit(): void {
@@ -30,4 +39,10 @@ export class BikeListComponent implements OnInit {
       this.countBikes = resp.totalElements;
     });
   }
+
+  openForm() {
+    this.dialog.open(FormAddBikeAdminComponent);
+  }
+
+
 }

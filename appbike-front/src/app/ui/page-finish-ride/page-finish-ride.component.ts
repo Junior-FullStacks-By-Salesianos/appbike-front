a66f174e39dcccd3382a9dd6b8babd2029418362
Cristian <pulidocabellochristian@gmail.com>
2023-11-29 12:43:24 +0100

    Para tomas

--- appbike-front/src/app/ui/page-finish-ride/page-finish-ride.component.ts
+++ appbike-front/src/app/ui/page-finish-ride/page-finish-ride.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { StationsService } from '../../services/stations.service';
+import { Station } from '../../models/list-all-stations';
+import { Loader } from '@googlemaps/js-api-loader';
+import { Router } from '@angular/router';
 import { UsoService } from '../../services/uso.service';
 import { UsoResponse } from '../../models/uso.interface';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-finish-ride',
@@ -10,18 +13,30 @@ import { Router } from '@angular/router';
 })
 export class PageFinishRideComponent implements OnInit {
 
+  stations: Station[] = [];
+  map: google.maps.Map | undefined;
+  markers: google.maps.Marker[] = [];
   uso!: UsoResponse;
   isLoading = true;
 
-  constructor(private usoService: UsoService, private router: Router) { }
-
+  constructor(
+    private stationService: StationsService,
+    private usoService: UsoService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.stationService.getAllStations().subscribe(resp => {
+      this.stations = resp;
+      this.initMap();
+    });
+
     this.usoService.getActiveUse().subscribe({
       next: resp => {
         this.uso = resp;
         this.isLoading = false;
-      }, error: err => {
+      },
+      error: err => {
         if (err.status == 404) {
           this.router.navigate(['/page-404'])
         }
@@ -29,6 +44,66 @@ export class PageFinishRideComponent implements OnInit {
     })
   }
 
+  showOnMap(): void {
+    if (!this.map) return;
 
+    this.markers.forEach(marker => {
+      marker.setMap(null);
+    });
+    this.markers = [];
 
+    this.stations.forEach(station => {
+
+      const stationNow = station.bikes;
+      let iconUrl = '';
+      let clickable = true;
+
+      if (stationNow > 0) {
+        iconUrl = 'assets/img/bikes.png';
+      } else {
+        iconUrl = 'assets/img/fullStation.png';
+        clickable = false;
+      }
+
+      const coordinates = station.coordinates.split(',');
+      const latitude = parseFloat(coordinates[0]);
+      const longitude = parseFloat(coordinates[1]);
+
+      const marker = new google.maps.Marker({
+        position: { lat: latitude, lng: longitude },
+        map: this.map,
+        title: station.name,
+        clickable: clickable,
+        icon: {
+          url: iconUrl,
+          scaledSize: new google.maps.Size(65, 65)
+        }
+      });
+
+      this.markers.push(marker);
+
+      if (clickable) {
+        marker.addListener('click', () => {
+          this.router.navigate(['/rentbystation', station.id]);
+        })
+      };
+    });
+  }
+
+  initMap(): void {
+    const loader = new Loader({
+      apiKey: "AIzaSyDtpcf5htnmyWhR26aWh9dEtyp9wqf2fxc",
+      version: "weekly"
+    });
+
+    loader.load().then(() => {
+      this.map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: { lat: 37.3891, lng: -5.9845 },
+        zoom: 14,
+      });
+      this.showOnMap();
+    }).catch(error => {
+      console.error("Error al cargar la API de Google Maps:", error);
+    });
+  }
 }
